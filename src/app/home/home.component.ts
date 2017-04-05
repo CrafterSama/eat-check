4ae0280e6066b5c43f2c9f2fb076a65ab761449b
Eli José Carrasquero <ielijose@gmail.com>
2017-04-05 17:52:02 -0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { AppState } from '../app.service';
 import { Title } from './title';
 import { XLargeDirective } from './x-large';
 
-declare var $:any;
+declare var $: any;
 
 @Component({
   // The selector is what angular internally uses
@@ -19,47 +19,36 @@ declare var $:any;
     Title
   ],
   // Our list of styles in our component. We may add more to compose many styles together
-  styleUrls: [ '../../assets/css/jquery.bxslider.css' ],
+  styleUrls: ['../../assets/css/jquery.bxslider.css'],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   // Set our default values
   public localState = { value: '' };
+  public place:number;
   // TypeScript public modifiers
   constructor(
     public appState: AppState,
     public title: Title
-  ) {}
+  ) { }
 
   public ngOnInit() {
     // this.title.getData().subscribe(data => this.data = data);
+	this.place = 0;
 
-    $( document ).ready(function() {
-       $('#overlay').fadeIn('fast',function(){
-               $('#box').animate({'top':'160px'},800);
-       });
-       $('.box select').change(function(){
-           $('#box').animate({'top':'-100%'},800,function(){
-               $('#overlay').fadeOut('fast');
-           });
-       });
-       $('.bxslider').bxSlider({
-           mode: 'fade',
-           controls:true,
-           captions: false,
-           pager: false
-       });
-       $('.slider-col').bxSlider({
-           slideWidth: 300,
-           minSlides: 1,
-           maxSlides: 7,
-           moveSlides: 1,
-           controls:true,
-           captions: false,
-           pager: false
-       });
-   });
+    $(document).ready(function() {
+      $('#overlay').fadeIn('fast', function() {
+        $('#box').animate({ 'top': '160px' }, 800);
+      });
+      $('.box select').change(function() {
+        $('#box').animate({ 'top': '-100%' }, 800, function() {
+          $('#overlay').fadeOut('fast');
+        });
+      });
+      $('.bxslider').bxSlider({ mode: 'fade', controls: true, captions: false, pager: false });
+      $('.slider-col').bxSlider({ slideWidth: 300, minSlides: 1, maxSlides: 7, moveSlides: 1, controls: true, captions: false, pager: false });
+    });
   }
 
   public submitState(value: string) {
